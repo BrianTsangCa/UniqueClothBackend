a00d600134dc414e4884f5ed7141fc3acfa5474b
BrianTsangCa <110198947+BrianTsangCa@users.noreply.github.com>
2024-11-19 14:37:55 -0800

    Product class build

---
+++ src/main/java/com/example/backend/Entity/Product.java
@@ -0,0 +1,23 @@
+package com.example.backend.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long product_id;
+
+    private String name;
+    private String description;
+    private String category;
+    private double price;
+    private int stock;
+    private String image_url;
+    private String created_at;
+    
+}
